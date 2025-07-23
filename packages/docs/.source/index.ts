32e8186a393c6f3c23e051225b4c61ee1e9e1606
rokki <hannahroksanne@gmail.com>
2025-07-23 05:56:04 -0500

    set up docs

---
+++ packages/docs/.source/index.ts
@@ -0,0 +1,6 @@
+// @ts-nocheck -- skip type checking
+import * as docs_1 from "../content/docs/test.mdx?collection=docs&hash=1753266631208"
+import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1753266631208"
+import { _runtime } from "fumadocs-mdx"
+import * as _source from "../source.config"
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"C:/Users/hannah/Desktop/codee/wem/packages/docs/content/docs/index.mdx"}, data: docs_0 }, { info: {"path":"test.mdx","absolutePath":"C:/Users/hannah/Desktop/codee/wem/packages/docs/content/docs/test.mdx"}, data: docs_1 }], [])
